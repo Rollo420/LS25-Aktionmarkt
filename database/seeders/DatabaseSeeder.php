f12369701259be57b6a437260b051e4a786463b7
javoss <jan.voss@tsbw.cloud>
2025-02-26 14:17:45 +0100

    "Added imports and functionality to DatabaseSeeder.php"

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,9 +5,15 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
+//My imports Model
 use App\Models\username;
 use App\Models\account;
 use App\Models\password;
+use App\Models\month;
+
+//My imports Seeder
+use Database\Seeders\monthSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,10 +22,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $monthSeeder = new monthSeeder();
 
         //username::factory(5)->create();
         password::factory(5)->create();
-        account::factory(5)->create();
+        account::factory(5)->create();                
+        $monthSeeder->CreateAllMonth();
 
         // User::factory(10)->create();
         //User::factory()->create([
