f419522c141a251e3f3c6ee7f4381c690d323417
javoss <jan.voss@tsbw.cloud>
2025-03-03 14:14:43 +0100

    `Update dependencies and configurations for Tailwind CSS and PostCSS`

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/postcss7-compat';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/scss/style.scss', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/scss/style.scss', 'resources/js/app.js'],
             refresh: true,
         }),
         tailwindcss(),
