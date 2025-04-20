7eb70958a44e21f9845a7993b55b9073d65b42c8
Rollo420 <peterhannes550@gmail.com>
2025-04-20 02:11:17 +0200

    configurate ddns

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,13 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0',
+        hmr: {
+            host: 'woodlypi.ddns.net',
+        },
+        watch: {
+            usePolling: true,
+        }
+    }
 });
