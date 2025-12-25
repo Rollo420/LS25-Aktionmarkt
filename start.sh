7bbabadcb759e01f9a91b7eeee29eed752f1c082
Rollo420 <peterhannes550@gmail.com>
2025-12-25 11:05:11 +0100

    `Update start.sh to use docker-compose with prod config`

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ sail='./vendor/bin/sail'
 
 git pull
 
-$sail up -d
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d 
 $sail artisan config:clear
 $sail npm run build
 #$sail npm run dev 
